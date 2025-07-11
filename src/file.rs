30c9c21cb1a294799c75c5c7a676b8b380d6bee7
Hossein Afkar <hossein.afkar@hotmail.com>
2025-07-12 01:52:55 +0330

    doc: added dots at the end of docs and comments

--- src/file.rs
+++ src/file.rs
@@ -2,43 +2,43 @@ use crate::Result;
 use crate::utils::Integer;
 use crate::Error;
 
-/// Elf file header type to store the file header information
+/// Elf file header type to store the file header information.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 pub struct FileHeader {
-    /// Elf bitness
+    /// Elf bitness.
     pub e_class: ElfClass,
-    /// Elf data encodings
+    /// Elf data encodings.
     pub e_data: ElfData,
-    /// Elf OS ABI
+    /// Elf OS ABI.
     pub e_abi: ElfOsAbi,
-    /// Elf file type
+    /// Elf file type.
     pub e_type: ElfType,
-    /// Elf machine ISA
+    /// Elf machine ISA.
     pub e_machine: ElfMachine,
-    /// Elf virtual address entry point
+    /// Elf virtual address entry point.
     pub e_entry: usize,
-    /// Pointer to the start of the program header table
+    /// Pointer to the start of the program header table.
     pub e_phoff: usize,
-    /// Pointer to the start of the section header table
+    /// Pointer to the start of the section header table.
     pub e_shoff: usize,
     /// Elf flags interpretation of this flag depends on the target
-    /// architecture
+    /// architecture.
     pub e_flags: u32,
-    /// Elf header size
+    /// Elf header size.
     pub e_ehsize: u16,
-    /// Elf program header table entry size
+    /// Elf program header table entry size.
     pub e_phentsize: u16,
-    /// Elf program header table entry size
+    /// Elf program header table entry size.
     pub e_phnum: u16,
-    /// Elf section header table entry size
+    /// Elf section header table entry size.
     pub e_shentsize: u16,
-    /// Elf Section header table entry count
+    /// Elf Section header table entry count.
     pub e_shnum: u16,
-    /// Elf section header string table index that contains section names
+    /// Elf section header string table index that contains section names.
     pub e_shstrndx: u16,
 }
 
-/// ElfClass specifies elf architecture and bitness
+/// ElfClass specifies elf architecture and bitness.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Default)]
 pub enum ElfClass {
     #[default]
@@ -47,7 +47,7 @@ pub enum ElfClass {
     Class64,
 }
 
-/// ElfData specifies elf data encodings
+/// ElfData specifies elf data encodings.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Default)]
 pub enum ElfData {
     #[default]
@@ -56,7 +56,7 @@ pub enum ElfData {
     ElfData2Msb,
 }
 
-/// ElfOSAbi specifies OS ABI of the elf file
+/// ElfOSAbi specifies OS ABI of the elf file.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Default)]
 pub enum ElfOsAbi {
     Sysv,
@@ -76,7 +76,7 @@ pub enum ElfOsAbi {
     Standalone,
 }
 
-/// ElfType defines elf object type
+/// ElfType defines elf object type.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Default)]
 pub enum ElfType {
     #[default]
@@ -89,9 +89,9 @@ pub enum ElfType {
     CpuSpecific,
 }
 
-/// ElfMachine Specifies machine ISA type
+/// ElfMachine Specifies machine ISA type.
 /// As this might get too big we will not specify all the available machine
-/// types in the libc
+/// types in the libc.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Default)]
 pub enum ElfMachine {
     #[default]
@@ -105,15 +105,16 @@ pub enum ElfMachine {
 }
 
 impl FileHeader {
-    /// Parse the elf header and populate the fields
+    /// Parse the elf header and populate the fields.
     pub fn parse(mut self, elf: &[u8]) -> Result<Self> {
-        // Get the elf magic number from the start of the file
+        // Get the elf magic number from the start of the file.
         if elf.get(0x00..0x04) != Some(b"\x7FELF") {
             return Err(Error::BadElf);
         }
 
-        // Check for the bitness of the elf file and report it back as ElfClass
-        // Option type is cloned to avoid storing numbers in the .data section
+        // Check for the bitness of the elf file and report it back as
+        // ElfClass. Option type is cloned to avoid storing numbers in
+        // the .data section.
         self.e_class = if elf.get(0x04) == Some(&1) {
             ElfClass::Class32
         } else if elf.get(0x04) == Some(&2) {
@@ -128,7 +129,7 @@ impl FileHeader {
             return Err(Error::UnsupportedClass);
         }
 
-        // Check the data encoding of the elf file
+        // Check the data encoding of the elf file.
         self.e_data = if elf.get(0x05) == Some(&1) {
             ElfData::ElfData2Lsb
         } else if elf.get(0x05) == Some(&2) {
@@ -137,12 +138,12 @@ impl FileHeader {
             ElfData::None
         };
 
-        // Check for the elf version currently this value must be 1
+        // Check for the elf version currently this value must be 1.
         if elf.get(0x06) != Some(&1) {
             return Err(Error::BadElf);
         }
 
-        // Check for the OS ABI
+        // Check for the OS ABI.
         self.e_abi = match elf.get(0x07) {
             Some(e) => match e {
                 0 => ElfOsAbi::Sysv,
@@ -165,13 +166,13 @@ impl FileHeader {
             }
         };
 
-        // Get the abi version but discard it for future
+        // Get the abi version but discard it for future.
         let _e_abi_ver = elf.get(0x08).unwrap_or(&0);
 
-        // Discard the padding byte at 0x09 to 0x10
+        // Discard the padding byte at 0x09 to 0x10.
         let _padding = elf.get(0x09..0x10).unwrap_or(&[0u8; 7]);
 
-        // Identify the elf type
+        // Identify the elf type.
         self.e_type = match elf.get(0x10..0x12) {
             Some(&[0x00, 0x00]) => ElfType::None,
             Some(&[0x01, 0x00]) => ElfType::Relocatable,
@@ -183,7 +184,7 @@ impl FileHeader {
             _ => ElfType::None,
         };
 
-        // Get the target ISA
+        // Get the target ISA.
         self.e_machine = match elf.get(0x12..0x14) {
             Some(&[0x03, 0x00]) => ElfMachine::Intel80386,
             Some(&[0x3e, 0x00]) => ElfMachine::Amd64,
@@ -194,114 +195,114 @@ impl FileHeader {
         };
 
         // Check for the elf version for another time apparently this has more
-        // options like none and current version which is 1
+        // options like none and current version which is 1.
         if elf.get(0x14..0x18) != Some(&[0x01, 0x00, 0x00, 0x00]) {
             return Err(Error::BadElf);
         }
 
-        //Now we need the parser position in file
+        //Now we need the parser position in file.
         let mut pos: usize = 0x18;
 
-        // Calculate the next header part ending
+        // Calculate the next header part ending.
         let mut next: usize = match self.e_class {
             ElfClass::Class64 => 0x08 + pos,
             ElfClass::Class32 => 0x04 + pos,
             ElfClass::None => 0x08 + pos,
         };
 
-        // This section depends on the elf file class so we branch out
+        // This section depends on the elf file class so we branch out.
         if self.e_class == ElfClass::Class64 || self.e_class == ElfClass::None
         {
-            // Get the elf virtual address entry point
+            // Get the elf virtual address entry point.
             self.e_entry = usize::endian_parse(pos..next, elf, &self.e_data)?;
 
-            // Move the position to the new header part
+            // Move the position to the new header part.
             pos = next;
 
-            // Calculate the next header part ending
+            // Calculate the next header part ending.
             next = pos + 0x08;
 
-            // Get the elf program header offset
+            // Get the elf program header offset.
             self.e_phoff = usize::endian_parse(pos..next, elf, &self.e_data)?;
 
-            // Move the position to the new header part
+            // Move the position to the new header part.
             pos = next;
 
-            // Calculate the next header part ending
+            // Calculate the next header part ending.
             next = pos + 0x08;
 
-            // Get the elf section header offset
+            // Get the elf section header offset.
             self.e_shoff = usize::endian_parse(pos..next, elf, &self.e_data)?;
         } else if self.e_class == ElfClass::Class32 {
-            // Get the elf virtual address entry point
+            // Get the elf virtual address entry point.
             self.e_entry =
                 u32::endian_parse(pos..next, elf, &self.e_data)? as usize;
 
-            // Move the position to the new header part
+            // Move the position to the new header part.
             pos = next;
 
-            // Calculate the next header part ending
+            // Calculate the next header part ending.
             next = pos + 0x04;
 
-            // Get the elf program header offset
+            // Get the elf program header offset.
             self.e_phoff =
                 u32::endian_parse(pos..next, elf, &self.e_data)? as usize;
 
-            // Move the position to the new header part
+            // Move the position to the new header part.
             pos = next;
 
-            // Calculate the next header part ending
+            // Calculate the next header part ending.
             next = pos + 0x04;
 
-            // Get the elf section header offset
+            // Get the elf section header offset.
             self.e_shoff =
                 u32::endian_parse(pos..next, elf, &self.e_data)? as usize;
         }
 
-        // Move the position to the new header part
+        // Move the position to the new header part.
         pos = next;
 
-        // Get the elf processor specific flags
+        // Get the elf processor specific flags.
         self.e_flags =
             u32::endian_parse(pos..(pos + 0x04), elf, &self.e_data)?;
 
-        // Move the position to the new header part
+        // Move the position to the new header part.
         pos += 0x04;
         self.e_ehsize =
             u16::endian_parse(pos..(pos + 0x02), elf, &self.e_data)?;
 
-        // Move the position to the new header part
+        // Move the position to the new header part.
         pos += 0x02;
 
-        // Get the elf program header entry size
+        // Get the elf program header entry size.
         self.e_phentsize =
             u16::endian_parse(pos..(pos + 0x02), elf, &self.e_data)?;
 
-        // Move the position to the new header part
+        // Move the position to the new header part.
         pos += 0x02;
 
-        // Get the elf program header entry size
+        // Get the elf program header entry size.
         self.e_phnum =
             u16::endian_parse(pos..(pos + 0x02), elf, &self.e_data)?;
 
-        // Move the position to the new header part
+        // Move the position to the new header part.
         pos += 0x02;
 
-        // Get the elf section header table entry size
+        // Get the elf section header table entry size.
         self.e_shentsize =
             u16::endian_parse(pos..(pos + 0x02), elf, &self.e_data)?;
 
-        // Move the position to the new header part
+        // Move the position to the new header part.
         pos += 0x02;
 
-        // Get the elf section header table entry count
+        // Get the elf section header table entry count.
         self.e_shnum =
             u16::endian_parse(pos..(pos + 0x02), elf, &self.e_data)?;
 
-        // Move the position to the new header part
+        // Move the position to the new header part.
         let pos: usize = pos + 0x02;
 
-        // Get the elf section header string table
+        // Get the elf section header string table.
         self.e_shstrndx =
             u16::endian_parse(pos..(pos + 0x02), elf, &self.e_data)?;
 
--- src/program.rs
+++ src/program.rs
@@ -1,4 +1,4 @@
-use crate::Result;
+use crate::{Result, Error};
 use crate::utils::Integer;
 use crate::file::{ElfData, ElfClass};
 
@@ -7,88 +7,88 @@ pub const PF_W: usize = 1 << 1;
 pub const PF_R: usize = 1 << 2;
 
 /// ProgramHeader stores information regarding to how the image sections
-/// should be laid out in the system memory
+/// should be laid out in the system memory.
 #[derive(Debug, Copy, Clone)]
 pub struct ProgramHeader {
-    /// Identifies the type of the segment
+    /// Identifies the type of the segment.
     pub p_type: ProgramType,
     /// Segment flags
     pub p_flags: Perm,
-    /// Offset of the segment in the file
+    /// Offset of the segment in the file.
     pub p_offset: usize,
-    /// Virtual address of the segment in memory
+    /// Virtual address of the segment in memory.
     pub p_vaddr: usize,
-    /// Reserved for the physical address in the memory
+    /// Reserved for the physical address in the memory.
     pub p_paddr: usize,
-    /// Size of the segment in bytes
+    /// Size of the segment in bytes.
     pub p_filesz: usize,
-    /// Size of the segment mapped in memory in bytes
+    /// Size of the segment mapped in memory in bytes.
     pub p_memsz: usize,
     /// Specifies alignment and should be integral power of 2 (1 and 0 are no
-    /// alignment)
+    /// alignment).
     pub p_align: usize,
 }
 
-/// Enum to identify the program header type
+/// Enum to identify the program header type.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum ProgramType {
     None,
-    /// Program header entry is unused
+    /// Program header entry is unused.
     PtNull,
-    /// Loadable program segment
+    /// Loadable program segment.
     PtLoad,
-    /// Dynamic linking information
+    /// Dynamic linking information.
     PtDynamic,
-    /// Program interpreter
+    /// Program interpreter.
     PtInterp,
-    /// Auxiliary information
+    /// Auxiliary information.
     PtNote,
-    /// Reserved (not sure for what)
+    /// Reserved (not sure for what).
     PtShlib,
-    /// Entry for the header table itself
+    /// Entry for the header table itself.
     PtPhdr,
-    /// Thread-local storage segment
+    /// Thread-local storage segment.
     PtTls,
-    /// GCC .eh_frame_hdr segment
+    /// GCC .eh_frame_hdr segment.
     PtGnuEhFrame,
-    /// Indicates stack executability
+    /// Indicates stack executability.
     PtGnuStack,
-    /// Read-only after relocation
+    /// Read-only after relocation.
     PtGnuRelro,
-    /// GNU property
+    /// GNU property.
     PtGnuProperty,
-    /// OS specific segment
+    /// OS specific segment.
     PtOs,
-    /// Processor specific segment
+    /// Processor specific segment.
     PtProc,
 }
 
-/// struct to represent RWX perms on the program header
+/// struct to represent RWX perms on the program header.
 /// The three booleans represented in the struct are Read, Write, Execute
-/// in order
-/// It is best that associated functions be used when using this struct
+/// in order.
+/// It is best that associated functions be used when using this struct.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct Perm(pub bool, pub bool, pub bool);
 
-/// Helper type to implement the iterator type on
+/// Helper type to implement the iterator type on [`ProgramHeader`].
 /// The best is for the [`crate::Elf::program_iter()`] function to be called
-/// on the elf struct
+/// on the elf struct.
 #[derive(Debug, Clone, Copy)]
 pub struct ProgramIterator<'a> {
-    /// Owned program header struct to address
+    /// Owned program header struct to address.
     program_header: ProgramHeader,
-    /// Program header offset in the elf file
+    /// Program header offset in the elf file.
     offset: usize,
-    /// Program header entry size
+    /// Program header entry size.
     phentsize: u16,
     /// Number of program header entries also used as
-    /// the index of the iteration
+    /// the index of the iteration.
     phnum: u16,
-    /// Elf class used for parsing
+    /// Elf class used for parsing.
     class: ElfClass,
-    /// Elf endianness used for parsing
+    /// Elf endianness used for parsing.
     data: ElfData,
-    /// A reference to the elf file
+    /// A reference to the elf file.
     elf: &'a [u8],
 }
 
@@ -99,7 +99,7 @@ impl Default for ProgramHeader {
 }
 
 impl ProgramHeader {
-    /// The default [`ProgramHeader`] constructor
+    /// The default [`ProgramHeader`] constructor.
     pub fn new() -> Self {
         ProgramHeader {
             p_type: ProgramType::None,
@@ -113,14 +113,14 @@ impl ProgramHeader {
         }
     }
 
-    /// Parse the [`ProgramHeader`] and populate the fields
+    /// Parse the [`ProgramHeader`] and populate the fields.
     pub fn parse(
         mut self,
         elf: &[u8],
         class: ElfClass,
         data: ElfData,
     ) -> Result<Self> {
-        // Get the segment type
+        // Get the segment type.
         self.p_type = match elf.get(0x00..0x04) {
             Some(&[0x00, 0x00, 0x00, 0x00]) => ProgramType::PtNull,
             Some(&[0x01, 0x00, 0x00, 0x00]) => ProgramType::PtLoad,
@@ -138,65 +138,74 @@ impl ProgramHeader {
             _ => ProgramType::None,
         };
 
-        // Branch and parse according to the elf architecture class
-        if class == ElfClass::Class32 {
-            // Get the program offset of the segment
-            self.p_offset =
-                u32::endian_parse(0x04..0x08, elf, &data)? as usize;
+        // Match and parse according to the elf architecture class.
+        match class {
+            ElfClass::Class32 => {
+                // Get the program offset of the segment.
+                self.p_offset =
+                    u32::endian_parse(0x04..0x08, elf, &data)? as usize;
 
-            // Get the program offset of the segment in the virtual memory
-            self.p_vaddr = u32::endian_parse(0x08..0x0c, elf, &data)? as usize;
+                // Get the program offset of the segment in the virtual memory.
+                self.p_vaddr =
+                    u32::endian_parse(0x08..0x0c, elf, &data)? as usize;
 
-            // Get the program offset of the segment in the physical memory
-            // This part is only relevant in systems without which use
-            // memory segmentation
-            self.p_paddr = u32::endian_parse(0x0c..0x10, elf, &data)? as usize;
+                // Get the program offset of the segment in the physical
+                // memory. This part is only relevant in systems without
+                // which use memory segmentation.
+                self.p_paddr =
+                    u32::endian_parse(0x0c..0x10, elf, &data)? as usize;
 
-            // Size of the file image segment in bytes
-            self.p_filesz =
-                u32::endian_parse(0x10..0x14, elf, &data)? as usize;
+                // Size of the file image segment in bytes.
+                self.p_filesz =
+                    u32::endian_parse(0x10..0x14, elf, &data)? as usize;
 
-            // Size of the segment mapped in the memory in bytes
-            self.p_memsz = u32::endian_parse(0x14..0x18, elf, &data)? as usize;
+                // Size of the segment mapped in the memory in bytes.
+                self.p_memsz =
+                    u32::endian_parse(0x14..0x18, elf, &data)? as usize;
 
-            // Get the memory permissions of the segment
-            let flags = u32::endian_parse(0x18..0x1c, elf, &data)? as usize;
-            self.p_flags.2 = flags & PF_X != 0;
-            self.p_flags.1 = flags & PF_W != 0;
-            self.p_flags.0 = flags & PF_R != 0;
+                // Get the memory permissions of the segment.
+                let flags =
+                    u32::endian_parse(0x18..0x1c, elf, &data)? as usize;
+                self.p_flags.2 = flags & PF_X != 0;
+                self.p_flags.1 = flags & PF_W != 0;
+                self.p_flags.0 = flags & PF_R != 0;
 
-            // Specifies alignment
-            // 0 and 1 specify no alignment otherwise it should be integral
-            // power of 2
-            self.p_align = u32::endian_parse(0x1c..0x20, elf, &data)? as usize;
-        } else if class == ElfClass::Class64 {
-            // Get the memory permissions of the segment
-            let flags = u32::endian_parse(0x04..0x08, elf, &data)?;
-            self.p_flags.2 = flags as usize & PF_X != 0;
-            self.p_flags.1 = flags as usize & PF_W != 0;
-            self.p_flags.0 = flags as usize & PF_R != 0;
+                // Specifies alignment.
+                // 0 and 1 specify no alignment
+                // otherwise it should be integral power of 2.
+                self.p_align =
+                    u32::endian_parse(0x1c..0x20, elf, &data)? as usize;
+            }
+            ElfClass::Class64 => {
+                // Get the memory permissions of the segment.
+                let flags = u32::endian_parse(0x04..0x08, elf, &data)?;
+                self.p_flags.2 = flags as usize & PF_X != 0;
+                self.p_flags.1 = flags as usize & PF_W != 0;
+                self.p_flags.0 = flags as usize & PF_R != 0;
 
-            // Get the program offset of the segment in the file image
-            self.p_offset = usize::endian_parse(0x08..0x10, elf, &data)?;
+                // Get the program offset of the segment in the file image.
+                self.p_offset = usize::endian_parse(0x08..0x10, elf, &data)?;
 
-            // Get the program offset of the segment in the virtual memory
-            self.p_vaddr = usize::endian_parse(0x10..0x18, elf, &data)?;
+                // Get the program offset of the segment in the virtual memory.
+                self.p_vaddr = usize::endian_parse(0x10..0x18, elf, &data)?;
 
-            // Get the program offset of the segment in the physical memory
-            // This part is only relevant in systems without which use
-            // memory segmentation
-            self.p_paddr = usize::endian_parse(0x18..0x20, elf, &data)?;
+                // Get the program offset of the segment in the physical
+                // memory. This part is only relevant in systems without
+                // which use memory segmentation.
+                self.p_paddr = usize::endian_parse(0x18..0x20, elf, &data)?;
 
-            // Size of the file image segment in bytes
-            self.p_filesz = usize::endian_parse(0x20..0x28, elf, &data)?;
+                // Size of the file image segment in bytes.
+                self.p_filesz = usize::endian_parse(0x20..0x28, elf, &data)?;
 
-            // Size of the segment mapped in the memory in bytes
-            self.p_memsz = usize::endian_parse(0x28..0x30, elf, &data)?;
+                // Size of the segment mapped in the memory in bytes.
+                self.p_memsz = usize::endian_parse(0x28..0x30, elf, &data)?;
 
-            // Specifies alignment
-            // 0 and 1 specify no alignment otherwise it should be integral
-            // power of 2
-            self.p_align = usize::endian_parse(0x30..0x38, elf, &data)?;
+                // Specifies alignment
+                // 0 and 1 specify no alignment otherwise it should be
+                // integral power of 2.
+                self.p_align = usize::endian_parse(0x30..0x38, elf, &data)?;
+            }
+            ElfClass::None => Err(Error::UnsupportedClass)?,
         }
 
         Ok(self)
@@ -204,17 +213,17 @@ impl ProgramHeader {
 }
 
 impl Perm {
-    /// Return if the section is readable
+    /// Return if the section is readable.
     pub fn is_read(self) -> bool {
         self.0
     }
 
-    /// Return if the section is writable
+    /// Return if the section is writable.
     pub fn is_write(self) -> bool {
         self.1
     }
 
-    /// Return if the section is executable
+    /// Return if the section is executable.
     pub fn is_exec(self) -> bool {
         self.2
     }
@@ -223,11 +232,11 @@ impl Perm {
 impl<'a> Iterator for ProgramIterator<'a> {
     type Item = ProgramHeader;
     fn next(&mut self) -> Option<Self::Item> {
-        // If the number of program headers is zero then abort the iterator
+        // If the number of program headers is zero then abort the iterator.
         if self.phnum == 0 {
             None
         } else {
-            // Parse the program header into the struct
+            // Parse the program header into the struct.
             self.program_header = self
                 .program_header
                 .parse(
@@ -238,10 +247,10 @@ impl<'a> Iterator for ProgramIterator<'a> {
                 )
                 .ok()?;
 
-            // Calculate the next offset for the next program header
+            // Calculate the next offset for the next program header.
             self.offset += self.phentsize as usize;
 
-            // Subtract one from the number of the program headers
+            // Subtract one from the number of the program headers.
             self.phnum -= 1;
 
             Some(self.program_header)
@@ -258,7 +267,7 @@ impl<'a> ProgramIterator<'a> {
         data: ElfData,
         elf: &'a [u8],
     ) -> Self {
-        // Construct a empty program header for the program iterator
+        // Construct a empty program header for the program iterator.
         let program = ProgramHeader::new();
 
         ProgramIterator {

f69ac9b13d1c390da173cad5a1332f1fe0caf6ce
Hossein Afkar <hossein.afkar@hotmail.com>
2025-07-12 01:55:24 +0330

    fix: Wrong values types on SectionHeader

--- src/section/mod.rs
+++ src/section/mod.rs
@@ -3,166 +3,165 @@ use crate::utils::Integer;
 use crate::file::{ElfData, ElfClass};
 
 pub mod symtab;
-pub use symtab::SymTab;
+pub use symtab::{SymTabIterator, SymTabEnt};
 
-/// Writable
+/// Writable.
 pub const SHF_WRITE: u32 = 1 << 0;
-/// Occupies memory during execution
+/// Occupies memory during execution.
 pub const SHF_ALLOC: u32 = 1 << 1;
-/// Executable
+/// Executable.
 pub const SHF_EXECINSTR: u32 = 1 << 2;
 /// Might be merged?
 pub const SHF_MERGE: u32 = 1 << 4;
-/// Contains nul-terminated strings
+/// Contains nul-terminated strings.
 pub const SHF_STRINGS: u32 = 1 << 5;
-/// `sh_info` contains SHT index
+/// `sh_info` contains SHT index.
 pub const SHF_INFO_LINK: u32 = 1 << 6;
-/// Preserve order after combining
+/// Preserve order after combining.
 pub const SHF_LINK_ORDER: u32 = 1 << 7;
-/// Non-standard OS specific handling
+/// Non-standard OS specific handling.
 pub const SHF_OS_NONCONFORMING: u32 = 1 << 8;
-/// Section is member of a group
+/// Section is member of a group.
 pub const SHF_GROUP: u32 = 1 << 9;
-/// Section hold thread-local data
+/// Section hold thread-local data.
 pub const SHF_TLS: u32 = 1 << 10;
-/// Section with compressed data
+/// Section with compressed data.
 pub const SHF_COMPRESSED: u32 = 1 << 11;
-/// OS-specific
+/// OS-specific.
 pub const SHF_MASKOS: u32 = 0x0ff00000;
-/// Processor-specific
+/// Processor-specific.
 pub const SHF_MASKPROC: u32 = 0xf0000000;
-/// Not to be GCed by linker
+/// Not to be GCed by linker.
 pub const SHF_GNU_RETAIN: u32 = 1 << 21;
-/// Special ordering requirement
+/// Special ordering requirement.
 pub const SHF_ORDERED: u32 = 1 << 30;
-/// Section is excluded unless referenced or allocated (Solaris)
+/// Section is excluded unless referenced or allocated (Solaris).
 pub const SHF_EXCLUDE: u32 = 1 << 31;
 
-/// Section header stores data about the sections of the elf file
+/// Section header stores data about the sections of the elf file.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 pub struct SectionHeader {
-    /// Identifies section name as indexes which is an offset of shstrtab
+    /// Identifies section name as indexes which is an offset of shstrtab.
     pub sh_name: u32,
-    /// Section type
+    /// Section type.
     pub sh_type: SectionType,
-    /// Section flags describe sections permissions in memory
+    /// Section flags describe sections permissions in memory.
     pub sh_flags: SectionFlags,
-    /// Section virtual address at the execution
-    pub sh_addr: usize,
-    /// Section offset at the file image
-    pub sh_offset: usize,
-    /// Section size in bytes
-    pub sh_size: usize,
+    /// Section virtual address at the execution.
+    pub sh_addr: u64,
+    /// Section offset at the file image.
+    pub sh_offset: u64,
+    /// Section size in bytes.
+    pub sh_size: u64,
     /// Contains the section index of the associated section and info depends
-    /// on the `sh_type`
+    /// on the `sh_type`.
     pub sh_link: u32,
-    /// Additional section information which also depends on the `sh_type`
+    /// Additional section information which also depends on the `sh_type`.
     pub sh_info: u32,
-    /// Section alignment which must be a power of two
-    pub sh_addralign: usize,
-    /// Entry size in bytes for fixed size sections otherwise it is zero
-    pub sh_entsize: usize,
-    /// Used to identify the section header number to be linked to shstrtab
-    pub sh_ndx: usize,
+    /// Section alignment which must be a power of two.
+    pub sh_addralign: u64,
+    /// Entry size in bytes for fixed size sections otherwise it is zero.
+    pub sh_entsize: u64,
+    /// Used to identify the section header number to be linked to shstrtab.
+    pub sh_ndx: u64,
 }
 
-/// Enum to identify section types
+/// Enum to identify section types.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Default)]
 pub enum SectionType {
     #[default]
     None,
-    /// Section Table entry unused
+    /// Section Table entry unused.
     ShtNull,
-    /// Program data
+    /// Program data.
     ShtProgBits,
-    /// Symbol table
+    /// Symbol table.
     ShtSymTab,
-    /// String table
+    /// String table.
     ShtStrTab,
-    /// Relocation entries with addends
+    /// Relocation entries with addends.
     ShtRela,
-    /// Symbol hash table
+    /// Symbol hash table.
     ShtHash,
-    /// Dynamic linking information
+    /// Dynamic linking information.
     ShtDynamic,
-    /// Notes
+    /// Notes.
     ShtNotes,
-    /// Program space with no data (bss)
+    /// Program space with no data (bss).
     ShtNoBits,
-    /// Relocation entries no addends
+    /// Relocation entries no addends.
     ShtRel,
-    /// Reserved
+    /// Reserved.
     ShtShlib,
-    /// Dynamic linker symbol table
+    /// Dynamic linker symbol table.
     ShtDynSym,
-    /// Array of constructors
+    /// Array of constructors.
     ShtInitArray,
-    /// Array of destructors
+    /// Array of destructors.
     ShtFInitArray,
-    /// Array of predestructors
+    /// Array of predestructors.
     ShtPreInitArray,
-    /// Section group
+    /// Section group.
     ShtGroup,
-    /// Extended section indices
+    /// Extended section indices.
     ShtSymTabShndx,
-    /// RELR relative relocations
+    /// RELR relative relocations.
     ShtRelr,
-    /// Number of defined types
+    /// Number of defined types.
     ShtNum,
-    /// Os Specific
+    /// Os Specific.
     ShtOs,
-    /// Gnu object attributes
+    /// Gnu object attributes.
     ShtGnuAttributes,
-    /// Gnu style hash tables
+    /// Gnu style hash tables.
     ShtGnuHash,
-    /// Gnu prelink library list
+    /// Gnu prelink library list.
     ShtGnuLibList,
-    /// Processor specific sections
+    /// Processor specific sections.
     ShtProc,
-    /// Application specific sections
+    /// Application specific sections.
     ShtUser,
 }
 
-/// SectionFlags tuple struct to implement some is_* functions on
+/// SectionFlags tuple struct to implement some is_* functions on.
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Default)]
-pub struct SectionFlags(usize);
+pub struct SectionFlags(u32);
 
 /// Helper type to implement the iterator type on
-/// The best is for the [`crate::Elf::section_iter()`] function to be called
-/// on the elf struct
+/// The best is for the [`Elf::section_iter()`](crate::Elf::section_iter())
+/// function to be called on the elf struct.
 #[derive(Debug, Clone, Copy)]
 pub struct SectionIterator<'a> {
-    /// Used to count the number of sections
+    /// Used to count the number of sections.
     ndx: usize,
-    /// Owned section header struct to address
+    /// Owned section header struct to address.
     section_header: SectionHeader,
-    /// Section header offset in the elf file
+    /// Section header offset in the elf file.
     offset: usize,
-    /// Section header entry size
+    /// Section header entry size.
     shentsize: u16,
-    /// Number of header entries also used as
-    /// the index of the iteration
+    /// Number of header entries also used as the index of the iteration.
     shnum: u16,
-    /// Elf class used for parsing
+    /// Elf class used for parsing.
     class: ElfClass,
-    /// Elf endianness used for parsing
+    /// Elf endianness used for parsing.
     data: ElfData,
-    /// A reference to the elf file
+    /// A reference to the elf file.
     elf: &'a [u8],
 }
 
 impl SectionHeader {
-    /// Parse the program header and populate the fields
+    /// Parse the program header and populate the fields.
     pub fn parse(
         mut self,
         elf: &[u8],
         class: ElfClass,
         data: ElfData,
     ) -> Result<Self> {
-        // Get the pointer to the name of section
+        // Get the pointer to the name of section.
         self.sh_name = u32::endian_parse(0x00..0x04, elf, &data)?;
 
-        // Get the section type
+        // Get the section type.
         self.sh_type = match elf.get(0x04..0x08) {
             Some(&[0x00, 0x00, 0x00, 0x00]) => SectionType::ShtNull,
             Some(&[0x01, 0x00, 0x00, 0x00]) => SectionType::ShtProgBits,
@@ -192,59 +191,58 @@ impl SectionHeader {
             _ => SectionType::None,
         };
 
-        // Branch and parse according to the elf architecture class
+        // Branch and parse according to the elf architecture class.
         if class == ElfClass::Class32 {
-            // Get the section flags
-            self.sh_flags.0 =
-                u32::endian_parse(0x08..0x0c, elf, &data)? as usize;
+            // Get the section flags.
+            self.sh_flags.0 = u32::endian_parse(0x08..0x0c, elf, &data)?;
 
-            // Get the virtual address of the section in memory
-            self.sh_addr = u32::endian_parse(0x0c..0x10, elf, &data)? as usize;
+            // Get the virtual address of the section in memory.
+            self.sh_addr = u32::endian_parse(0x0c..0x10, elf, &data)? as u64;
 
-            // Get the offset of the section in elf file image
-            self.sh_offset =
-                u32::endian_parse(0x10..0x14, elf, &data)? as usize;
+            // Get the offset of the section in elf file image.
+            self.sh_offset = u32::endian_parse(0x10..0x14, elf, &data)? as u64;
 
-            // Get the size of the section in the elf file image in bytes
-            self.sh_size = u32::endian_parse(0x14..0x18, elf, &data)? as usize;
+            // Get the size of the section in the elf file image in bytes.
+            self.sh_size = u32::endian_parse(0x14..0x18, elf, &data)? as u64;
 
-            // Get the section index
+            // Get the section index.
             self.sh_link = u32::endian_parse(0x18..0x1c, elf, &data)?;
 
-            // Get the section extra info
+            // Get the section extra info.
             self.sh_info = u32::endian_parse(0x1c..0x20, elf, &data)?;
 
-            // Get the section alignment
+            // Get the section alignment.
             self.sh_addralign =
-                u32::endian_parse(0x20..0x24, elf, &data)? as usize;
+                u32::endian_parse(0x20..0x24, elf, &data)? as u64;
 
-            // Get the entry size in bytes for sections that contain entries
+            // Get the entry size in bytes for sections that contain entries.
             self.sh_entsize =
-                u32::endian_parse(0x24..0x28, elf, &data)? as usize;
+                u32::endian_parse(0x24..0x28, elf, &data)? as u64;
         } else if class == ElfClass::Class64 {
-            // Get the section flags
-            self.sh_flags.0 = usize::endian_parse(0x08..0x10, elf, &data)?;
+            // Get the section flags.
+            self.sh_flags.0 =
+                u64::endian_parse(0x08..0x10, elf, &data)? as u32;
 
-            // Get the virtual address of the section in memory
-            self.sh_addr = usize::endian_parse(0x10..0x18, elf, &data)?;
+            // Get the virtual address of the section in memory.
+            self.sh_addr = u64::endian_parse(0x10..0x18, elf, &data)?;
 
-            // Get the offset of the section in elf file image
-            self.sh_offset = usize::endian_parse(0x18..0x20, elf, &data)?;
+            // Get the offset of the section in elf file image.
+            self.sh_offset = u64::endian_parse(0x18..0x20, elf, &data)?;
 
-            // Get the size of the section in the elf file image in bytes
-            self.sh_size = usize::endian_parse(0x20..0x28, elf, &data)?;
+            // Get the size of the section in the elf file image in bytes.
+            self.sh_size = u64::endian_parse(0x20..0x28, elf, &data)?;
 
-            // Get the section index
+            // Get the section index.
             self.sh_link = u32::endian_parse(0x28..0x2c, elf, &data)?;
 
-            // Get the section extra info
+            // Get the section extra info.
             self.sh_info = u32::endian_parse(0x2c..0x30, elf, &data)?;
 
-            // Get the section alignment
-            self.sh_addralign = usize::endian_parse(0x30..0x38, elf, &data)?;
+            // Get the section alignment.
+            self.sh_addralign = u64::endian_parse(0x30..0x38, elf, &data)?;
 
-            // Get the entry size in bytes for sections that contain entries
-            self.sh_entsize = usize::endian_parse(0x38..0x40, elf, &data)?;
+            // Get the entry size in bytes for sections that contain entries.
+            self.sh_entsize = u64::endian_parse(0x38..0x40, elf, &data)?;
         }
 
         Ok(self)
@@ -253,48 +251,48 @@ impl SectionHeader {
 
 impl SectionFlags {
     pub fn is_write(self) -> bool {
-        self.0 & SHF_WRITE as usize == SHF_WRITE as usize
+        self.0 & SHF_WRITE == SHF_WRITE
     }
     pub fn is_alloc(self) -> bool {
-        self.0 & SHF_ALLOC as usize == SHF_ALLOC as usize
+        self.0 & SHF_ALLOC == SHF_ALLOC
     }
     pub fn is_exec(self) -> bool {
-        self.0 & SHF_EXECINSTR as usize == SHF_EXECINSTR as usize
+        self.0 & SHF_EXECINSTR == SHF_EXECINSTR
     }
     pub fn is_merge(self) -> bool {
-        self.0 & SHF_MERGE as usize == SHF_MERGE as usize
+        self.0 & SHF_MERGE == SHF_MERGE
     }
     pub fn is_strings(self) -> bool {
-        self.0 & SHF_STRINGS as usize == SHF_STRINGS as usize
+        self.0 & SHF_STRINGS == SHF_STRINGS
     }
     pub fn is_info_link(self) -> bool {
-        self.0 & SHF_INFO_LINK as usize == SHF_INFO_LINK as usize
+        self.0 & SHF_INFO_LINK == SHF_INFO_LINK
     }
     pub fn is_link_order(self) -> bool {
-        self.0 & SHF_LINK_ORDER as usize == SHF_LINK_ORDER as usize
+        self.0 & SHF_LINK_ORDER == SHF_LINK_ORDER
     }
     pub fn is_os_nonconforming(self) -> bool {
-        self.0 & SHF_OS_NONCONFORMING as usize == SHF_OS_NONCONFORMING as usize
+        self.0 & SHF_OS_NONCONFORMING == SHF_OS_NONCONFORMING
     }
     pub fn is_group(self) -> bool {
-        self.0 & SHF_GROUP as usize == SHF_GROUP as usize
+        self.0 & SHF_GROUP == SHF_GROUP
     }
     pub fn is_tls(self) -> bool {
-        self.0 & SHF_TLS as usize == SHF_TLS as usize
+        self.0 & SHF_TLS == SHF_TLS
     }
     pub fn is_compressed(self) -> bool {
-        self.0 & SHF_COMPRESSED as usize == SHF_COMPRESSED as usize
+        self.0 & SHF_COMPRESSED == SHF_COMPRESSED
     }
 }
 
 impl<'a> Iterator for SectionIterator<'a> {
     type Item = SectionHeader;
     fn next(&mut self) -> Option<Self::Item> {
-        // If the number of section headers is zero then abort the iterator
+        // If the number of section headers is zero then abort the iterator.
         if self.shnum == 0 {
             None
         } else {
-            // Parse the section header into the struct
+            // Parse the section header into the struct.
             self.section_header = self
                 .section_header
                 .parse(
@@ -305,14 +303,14 @@ impl<'a> Iterator for SectionIterator<'a> {
                 )
                 .ok()?;
 
-            // Calculate the next offset for the next program header
+            // Calculate the next offset for the next program header.
             self.offset += self.shentsize as usize;
 
-            // Subtract one from the number of the program headers
+            // Subtract one from the number of the program headers.
             self.shnum -= 1;
 
-            // Set the index number
-            self.section_header.sh_ndx = self.ndx;
+            // Set the index number.
+            self.section_header.sh_ndx = self.ndx as u64;
             self.ndx += 1;
 
             Some(self.section_header)
@@ -329,7 +327,7 @@ impl<'a> SectionIterator<'a> {
         data: ElfData,
         elf: &'a [u8],
     ) -> Self {
-        // Construct a empty section header for the program iterator
+        // Construct a empty section header for the program iterator.
         let section = SectionHeader::default();
 
         SectionIterator {

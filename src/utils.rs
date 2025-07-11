0a349c6baf217b03d523784b52892bd09a8b0154
Hossein Afkar <hossein.afkar@hotmail.com>
2025-07-11 23:48:09 +0330

    ref: changed the util Integer trait into a macro

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use crate::file::ElfData;
+use crate::Error;
 
 pub trait Integer: Sized {
     fn endian_parse(
@@ -8,59 +9,32 @@ pub trait Integer: Sized {
     ) -> crate::Result<Self>;
 }
 
-impl Integer for usize {
-    fn endian_parse(
-        range: core::ops::Range<usize>,
-        bytes: &[u8],
-        e_data: &ElfData,
-    ) -> crate::Result<Self> {
-        let arr = bytes
-            .get(range)
-            .ok_or(crate::Error::OffsetCalculationFailure)?
-            .try_into()
-            .map_err(|_err| crate::Error::OffsetCalculationFailure)?;
-        Ok(match e_data {
-            ElfData::ElfData2Lsb => usize::from_le_bytes(arr),
-            ElfData::ElfData2Msb => usize::from_be_bytes(arr),
-            ElfData::None => usize::from_le_bytes(arr),
-        })
-    }
+macro_rules! impl_integer {
+    ($t:ty) => {
+        impl Integer for $t {
+            fn endian_parse(
+                range: core::ops::Range<usize>,
+                bytes: &[u8],
+                e_data: &ElfData,
+            ) -> crate::Result<Self> {
+                let arr: [u8; core::mem::size_of::<$t>()] = bytes
+                    .get(range)
+                    .ok_or(Error::OffsetCalculationFailure)?
+                    .try_into()
+                    .map_err(|_| Error::OffsetCalculationFailure)?;
+
+                Ok(match e_data {
+                    ElfData::ElfData2Lsb | ElfData::None => {
+                        <$t>::from_le_bytes(arr)
+                    }
+                    ElfData::ElfData2Msb => <$t>::from_be_bytes(arr),
+                })
+            }
+        }
+    };
 }
 
-impl Integer for u32 {
-    fn endian_parse(
-        range: core::ops::Range<usize>,
-        bytes: &[u8],
-        e_data: &ElfData,
-    ) -> crate::Result<Self> {
-        let arr = bytes
-            .get(range)
-            .ok_or(crate::Error::OffsetCalculationFailure)?
-            .try_into()
-            .map_err(|_err| crate::Error::OffsetCalculationFailure)?;
-        Ok(match e_data {
-            ElfData::ElfData2Lsb => u32::from_le_bytes(arr),
-            ElfData::ElfData2Msb => u32::from_be_bytes(arr),
-            ElfData::None => u32::from_le_bytes(arr),
-        })
-    }
-}
-
-impl Integer for u16 {
-    fn endian_parse(
-        range: core::ops::Range<usize>,
-        bytes: &[u8],
-        e_data: &ElfData,
-    ) -> crate::Result<Self> {
-        let arr = bytes
-            .get(range)
-            .ok_or(crate::Error::OffsetCalculationFailure)?
-            .try_into()
-            .map_err(|_err| crate::Error::OffsetCalculationFailure)?;
-        Ok(match e_data {
-            ElfData::ElfData2Lsb => u16::from_le_bytes(arr),
-            ElfData::ElfData2Msb => u16::from_be_bytes(arr),
-            ElfData::None => u16::from_le_bytes(arr),
-        })
-    }
-}
+impl_integer!(u8);
+impl_integer!(u16);
+impl_integer!(u32);
+impl_integer!(usize);
